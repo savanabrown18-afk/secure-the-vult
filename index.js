d9ea773f851e996c56d68a148e54979a198d0e2e
savanabrown18-afk <savanabrown18@gmail.com>
2026-04-26 16:10:35 -0600

    Add files via upload
    added readme, index.html, and index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+let code1;
+code1 = 7 + 3;
+const code2 = 100 - 120 / 2;
+const code3 = 39 % 100;
+const message = "The vult has been secured! The combination is ";
+const codeA = code1 + "-" + code2 + "-" + code3;
+const codeA2 = code1 + " - " + code2 + " - " + code3;
+const codeB = `${code1}-${code2}-${code3}`;
+
+const stringWithMultipleLines = `
+hello
+this is a
+poem`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
